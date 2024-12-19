64e93e836d8d14a6d3c734d15d1006dfe6ec1fd5
sarvar <sarlion.98@mail.ru>
2024-12-19 05:48:33 +0300

    Add project files.

---
+++ CreateWebAPI/Program.cs
@@ -0,0 +1,50 @@
+using CreateWebAPI.Extensions;
+using LoggerService;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+namespace CreateWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+            // Add services to the container.
+            builder.Services.ConfigureCors();
+            builder.Services.ConfigureIISIntegration();
+            builder.Services.ConfigureLoggerService();
+            builder.Services.ConfigureRepositoryManager();
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+                // Configure the HTTP request pipeline.
+
+                app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.All
+            });
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Repository/RepositoryManager .cs
@@ -0,0 +1,26 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class RepositoryManager: IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+        private readonly Lazy<ICompanyRepository> _CompanyRepository;
+        private readonly Lazy<IEmployeeRepository> _EmployeeRepository;
+
+        public RepositoryManager(RepositoryContext context, Lazy<ICompanyRepository> companyRepository, Lazy<IEmployeeRepository> employeeRepository)
+        {
+            _context = context;
+            _CompanyRepository = new Lazy<ICompanyRepository>(()=>new CompanyRepository(_context));
+            _EmployeeRepository = new Lazy<IEmployeeRepository>(()=>new EmployeeRepository(_context));
+        }
+        public ICompanyRepository CompanyRepository => _CompanyRepository.Value;
+        public IEmployeeRepository EmployeeRepository => _EmployeeRepository.Value;
+        public void Save()=> _context.SaveChanges();
+    }
+}
